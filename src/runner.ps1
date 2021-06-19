06ef223373e58c75b41b3da765c8c8a925aa5b42
adiswami14 <adithya1431@gmail.com>
2021-06-19 00:12:00 -0400

    Create a basic setup script

--- src/runner.ps1
+++ src/runner.ps1
@@ -3,21 +3,22 @@
 while($true) {
     $command = Read-Host -Prompt "Enter command" # Get command from powershell
     $command_arr = $command.Split(" ")
+    $command_word = $command_arr[0].ToLower()
 
-    if($command_arr[0].ToLower() -eq "quit") {
+    if($command_word -eq "quit") {
         break
-    } elseif ($command_arr[0].ToLower() -eq "search") {
+    } elseif ($command_word -eq "search") {
         sh chrome_runner.sh $command_arr[1..($command_arr.Length - 1)]
-    } elseif ($command_arr[0].ToLower() -eq "play") {
+    } elseif ($command_word -eq "play") {
         #Start-Process -FilePath "/Applications/"
         Find-Module -Name "PSSpotify"
         if($?) {
             Write-Host -Object "Module Found!"
         }
         # Actual playing and everything should go here
-    } elseif ($command_arr[0].ToLower() -eq "status") {
+    } elseif ($command_word -eq "status") {
         sh status_checker.sh
-    } elseif ($command_arr[0].ToLower() -eq "open") {
+    } elseif ($command_word -eq "open") {
          # Recurse to find projects
         python project_opener.py $command_arr[1..($command_arr.Length - 1)]
     } else {
